dedae34c71d8d783f21086312dc4f07fcd601cf3
Xsanzz-13 <davidnihboss684@gmail.com>
2026-09-19 08:21:45 +0700

    fix: restore valid cluster check

--- drivers/cpufreq/sprd-cpufreqhw.c
+++ drivers/cpufreq/sprd-cpufreqhw.c
@@ -40,8 +40,8 @@ static ssize_t sprd_uv_mV_table_show(struct cpufreq_policy *policy,
 
         cluster = topology_physical_package_id(policy->cpu);
 
-        if (cluster < HWDVFS_CHNL00 ||
-            cluster >= HWDVFS_CHNL_MAX)
+        if (cluster < 0 ||
+            cluster >= SPRD_CPUFREQ_MAX_CLUSTER)
                 return -EINVAL;
 
         return sprd_hwdvfs_l3_uv_get_table(cluster, buf, PAGE_SIZE);
@@ -58,8 +58,8 @@ static ssize_t sprd_uv_mV_table_store(struct cpufreq_policy *policy,
 
         cluster = topology_physical_package_id(policy->cpu);
 
-        if (cluster < HWDVFS_CHNL00 ||
-            cluster >= HWDVFS_CHNL_MAX)
+        if (cluster < 0 ||
+            cluster >= SPRD_CPUFREQ_MAX_CLUSTER)
                 return -EINVAL;
 
         if (!cpufreq_datas[cluster] ||
